c0e2364ffa8dfe4b3bf7e964d37a7bd5274f4e5d
Coralie RACHEX <rachex.coralie@gmail.com>
2018-01-05 10:05:21 +0100

    Fix shortcuts

--- src/main-window.cc
+++ src/main-window.cc
@@ -274,13 +274,13 @@ void CMainWindow::createActions()
   connect(m_loadProfileAct, SIGNAL(triggered()), SLOT(loadProfile()));
 
   m_previousFileAct = new QAction(tr("&Previous"), this);
-  m_previousFileAct->setShortcut(Qt::CTRL + QKeySequence::MoveToPreviousChar);
+    m_previousFileAct->setShortcut(Qt::CTRL + Qt::Key_Left);
   m_previousFileAct->setIcon(QIcon::fromTheme("go-previous", QIcon(":/icons/tango/48x48/go-previous.png")));
   m_previousFileAct->setStatusTip(tr("Load previous data file in current folder"));
   connect(m_previousFileAct, SIGNAL(triggered()), SLOT(previousFile()));
 
   m_nextFileAct = new QAction(tr("&Next"), this);
-  m_nextFileAct->setShortcut(Qt::CTRL + QKeySequence::MoveToNextChar);
+  m_nextFileAct->setShortcut(Qt::CTRL + Qt::Key_Right);
   m_nextFileAct->setIcon(QIcon::fromTheme("go-next", QIcon(":/icons/tango/48x48/go-next.png")));
   m_nextFileAct->setStatusTip(tr("Load next data file in current folder"));
   connect(m_nextFileAct, SIGNAL(triggered()), SLOT(nextFile()));
@@ -440,6 +440,7 @@ void CMainWindow::toggleImageView(bool value)
     if (view != nullptr)
     {
       view->setVisible(value);
+      view->setFocus(Qt::OtherFocusReason);
     }
   }
 }
